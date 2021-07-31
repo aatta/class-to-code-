39e17a52031c6e1d73b5b9f76430b24f356f5edc
Abdullah Atta <abdullahatta@gmail.com>
2021-07-31 15:20:59 +0500

    adjustment

--- DiagramDesigner/Helpers/ToolBoxData.cs
+++ DiagramDesigner/Helpers/ToolBoxData.cs
@@ -11,11 +11,15 @@ namespace DiagramDesigner.Helpers
         public Type Type { get; private set; }
 
         public string ToolTip { get; private set; }
+        public double Height { get; private set; }
+        public double Width { get; private set; }
 
-        public ToolBoxData(string imageUrl, Type type)
+        public ToolBoxData(string imageUrl, Type type, double width, double height)
         {
             this.ImageUrl = imageUrl;
             this.Type = type;
+            this.Width = width;
+            this.Height = height;
         }
     }
 }
