69b48b719ff6cebde6e61ef3922f47f03acee25b
Abdullah Atta <abdullahatta@gmail.com>
2021-10-04 15:50:43 +0500

    Code generator implementation.

--- CD2C/Services/Contracts/ICodeGeneratorService.cs
+++ CD2C/Services/Contracts/ICodeGeneratorService.cs
@@ -1,9 +1,12 @@
-﻿using DiagramDesigner;
+﻿using CD2C.Common;
+using DiagramDesigner;
+using System.Collections.Generic;
 
 namespace CD2C
 {
     public interface ICodeGeneratorService
     {
         string GenerateCode(DiagramViewModel diagramViewModel);
+        string GenerateClass(string className, IEnumerable<MethodModel> methods, IEnumerable<DataMemberModel> dataMembers);
     }
 }
